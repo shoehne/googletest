3546bca8e75afc2c9234711f0815ce3ab1de7a84
shoehne <sven.hoehne@hotmail.com>
2024-02-05 22:49:00 +0100

    Updated premake file by adding project build instructions for the different build configurations.

--- premake5.lua
+++ premake5.lua
@@ -4,6 +4,13 @@ project "GoogleTest"
     cppdialect "c++20"
     location "./"
 
+    configurations {
+
+		"Debug",
+		"Release",
+		"Distribution"
+	}
+
     targetdir ("../../bin/" .. output_dir .. "/%{prj.name}")
     objdir ("../../bin-obj/" .. output_dir .. "/%{prj.name}")
 
@@ -21,19 +28,43 @@ project "GoogleTest"
         
     }
 
-    filter {"system:windows", "configurations:Debug"}
+    filter "system:windows"
         systemversion "latest"
         staticruntime "on"
-        buildoptions {
+    
+    filter "configurations:Debug"
+		defines "DEBUG"
+		runtime "Debug"
+		symbols "on"
+		filter {"configurations:Debug", "system:windows"}
+			buildoptions {"/MTd"}
 
-            "/MTd"
-        }
+	filter "configurations:Release"
+		defines "RELEASE"
+		runtime "Release"
+		optimize "on"
+		filter {"configurations:Release", "system:windows"}
+			buildoptions {"/MT"}
+
+	filter "configurations:Distribution"
+		defines "DISTRIBUTION"
+		runtime "Release"
+		optimize "on"
+		filter {"configurations:Distribution", "system:windows"}
+			buildoptions {"/MT"}
 
 project "GoogleMock"
     kind "StaticLib"
     language "C++"
     cppdialect "c++20"
     location "./"
+
+    configurations {
+
+		"Debug",
+		"Release",
+		"Distribution"
+	}
 
     targetdir ("../../bin/" .. output_dir .. "/%{prj.name}")
     objdir ("../../bin-obj/" .. output_dir .. "/%{prj.name}")
@@ -51,10 +82,26 @@ project "GoogleMock"
         "./googletest/include"
     }
 
-    filter {"system:windows", "configurations:Debug"}
+    filter "system:windows"
         systemversion "latest"
         staticruntime "on"
-        buildoptions {
+    filter "configurations:Debug"
+		defines "DEBUG"
+		runtime "Debug"
+		symbols "on"
+		filter {"configurations:Debug", "system:windows"}
+			buildoptions {"/MTd"}
 
-            "/MTd"
-        }
+	filter "configurations:Release"
+		defines "RELEASE"
+		runtime "Release"
+		optimize "on"
+		filter {"configurations:Release", "system:windows"}
+			buildoptions {"/MT"}
+
+	filter "configurations:Distribution"
+		defines "DISTRIBUTION"
+		runtime "Release"
+		optimize "on"
+		filter {"configurations:Distribution", "system:windows"}
+			buildoptions {"/MT"}
